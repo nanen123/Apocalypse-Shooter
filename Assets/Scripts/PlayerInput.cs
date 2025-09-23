214f411f8331b3bd9f0ce34eb199493292587421
nanen123 <lkjhgf13579@naver.com>
2025-09-24 00:21:18 +0900

    이동 기초 구현
    InputSystem 사용법 숙지

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
+    private Rigidbody rig;
+    private Vector3 moveVector;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        rig.linearVelocity = moveVector * 3;
+    }
+    public void OnMove(InputValue value)
+    {
+        Debug.Log(value.Get<Vector3>());
+        moveVector = value.Get<Vector3>();
     }
 }
