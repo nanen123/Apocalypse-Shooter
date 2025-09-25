603ed090b8a1e92d1c6d5a39889a59fb6373c4ec
nanen123 <lkjhgf13579@naver.com>
2025-09-25 23:01:10 +0900

    0925 작업 목록
    TPS 카메라 시점 구현 완료
    카메라 기준 이동 구현 완료
    
    구현해야 하는 것
    1. 이동 관련
    - 이동하는 방향으로 회전
    2. 적
    3. 총 발사

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -3,21 +3,50 @@ using UnityEngine.InputSystem;
 
 public class CameraMove : MonoBehaviour
 {
-    [SerializeField] private InputAction mouseXMoveAction;
-    [SerializeField] private InputAction mouseYMoveAction;
-    [SerializeField] private Vector2 mouseAxis;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private InputAction mouseXMoveAction;
+    private InputAction mouseYMoveAction;
+
+    private float rotX; // ∏∂øÏΩ∫ ¿Œ«≤
+    private float rotY;
+
+    public Transform objectTofollow;
+    public float sensitivity;
+    public float clampAngle;
+
+    public Transform cam;
+    private Vector3 dirNormalized; // πÊ«‚ ∫§≈Õ
+    private Vector3 finalDir;
+    public float minDistance;
+    public float maxDistance;
+    public float finalDistance;
+
+    void Awake()
     {
         mouseXMoveAction = InputSystem.actions.FindAction("MouseXAxis");
         mouseYMoveAction = InputSystem.actions.FindAction("MouseYAxis");
+
+        rotX = transform.localRotation.eulerAngles.x;
+        rotY = transform.localRotation.eulerAngles.y;
+
+        dirNormalized = cam.localPosition.normalized;
+        finalDistance = cam.localPosition.magnitude;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        mouseAxis = new Vector2(mouseXMoveAction.ReadValue<float>(), mouseYMoveAction.ReadValue<float>());
-        Debug.Log(mouseAxis);
+        rotX -= mouseYMoveAction.ReadValue<float>() * sensitivity * Time.deltaTime;
+        rotY += mouseXMoveAction.ReadValue<float>() * sensitivity * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        Quaternion rot = Quaternion.Euler(rotX, rotY, 0);
+
+        transform.rotation = rot;
     }
 
+    private void LateUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, objectTofollow.position, 10f * Time.deltaTime);
+
+        finalDir = transform.TransformPoint(dirNormalized * maxDistance);
+        cam.localPosition = Vector3.Lerp(cam.localPosition, dirNormalized * finalDistance, Time.deltaTime);
+    }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,18 +4,34 @@ using UnityEngine.InputSystem;
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody rig;
-    private Vector3 moveVector;
-    private bool isFiring;
     [SerializeField] private InputAction moveAction;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private Vector3 moveVector;
+    public float moveSpeed;
+
+    [SerializeField] private Vector3 rotVector; // 움직일 때 회전을 위한 벡터
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private bool isFiring;
+
+    [SerializeField] public Transform cam;
+    public Vector3 temp;
+
     void Awake()
     {
         rig = GetComponent<Rigidbody>();
         moveAction = InputSystem.actions.FindAction("Move");
     }
+
     private void Update()
     {
-        rig.linearVelocity = moveAction.ReadValue<Vector3>();
+        moveVector = moveAction.ReadValue<Vector3>();
+        Vector3 dir = (cam.transform.localRotation * Vector3.forward) * moveVector.z + (cam.transform.localRotation * Vector3.right) * moveVector.x;
+        Debug.Log(dir);
+        rig.linearVelocity = dir * moveSpeed;
+
+        //rotVector = transform.position + moveVector;
+        //if (moveVector != Vector3.zero)
+        //    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rotVector - transform.position), Time.deltaTime * rotSpeed);
     }
 
 }
